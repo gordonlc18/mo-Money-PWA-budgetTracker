b6b2c28d8afc80bf716a25e9dc95d0a63f1a002a
gordonlc18 <gordonlc18@gmail.com>
2022-06-03 23:54:05 -0400

    Added db connection file, .env and dotenv dependency to hide database secret infomation.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const db = require('./initDB') ();
+require('dotenv').config()
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,13 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
 
 // routes
 app.use(require("./routes/api.js"));
